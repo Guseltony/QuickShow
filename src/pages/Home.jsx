ea5c1c96c99975ffbcc45b9268e78a1dea6c284c
Akanji Anthony Guseltony <devguselt@gmail.com>
2025-12-11 11:46:54 +0100

    implemented the movie feactures section at the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import Featuredsection from '../components/Featuredsection'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <Featuredsection />
     </>
   )
 }
