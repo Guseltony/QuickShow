b27a509e28efff0e16609e05d63d875ebf816bcd
Akanji Anthony Guseltony <devguselt@gmail.com>
2025-12-12 19:50:43 +0100

    implemented the seatLayout page for booking cinema seat

--- src/pages/SeatLayout.jsx
+++ src/pages/SeatLayout.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
-import { ClockIcon } from 'lucide-react'
+import { assets, dummyDateTimeData, dummyShowsData } from '../assets/assets'
+import { ArrowRightIcon, ClockIcon } from 'lucide-react'
 import Loading from '../components/Loading'
 import isoTimeFormat from '../lib/isoTimeFormat'
+import BlurCircle from '../components/BlurCircle'
+import toast from 'react-hot-toast'
 
 const SeatLayout = () => {
+
+  const groupRows = [['A', 'B'],['C', 'D'],['E', 'F'],['G', 'H'],['I', 'J'],]
 
   const { id, date } = useParams()
   
@@ -25,6 +29,38 @@ const SeatLayout = () => {
     }
   }
 
+  const handleSeatClick = (seatId) => {
+    if (!selectedTime) {
+      return toast('Please selected time first')
+    }
+
+    if (!selectedSeats.includes(seatId) && selectedSeats.length > 4) {
+      return toast('You can only select 5 seats')
+    }
+
+    setSelectedSeats(prev => prev.includes(seatId) ? prev.filter(seat => seat !== seatId) : [...prev, seatId])
+  }
+
+  const renderSeats = (row, count = 9) => (
+    <div key={row} className='flex gap-2 mt-2'>
+      <div className='flex flex-wrap items-center justify-center gap-2'>
+        {
+          Array.from({ length: count }, (_, i) => {
+            const seatId = `${row}${i + 1}`
+            return (
+              <button
+                key={seatId}
+                onClick={() => handleSeatClick(seatId)}
+                className={`h-8 w-8 rounded border border-primary/60 cursor-pointer ${selectedSeats.includes(seatId) && 'bg-primary text-white'}`}>
+                {seatId}
+              </button>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+
   useEffect(() => {
     getShow()
   }, [])
@@ -36,7 +72,7 @@ const SeatLayout = () => {
         <p className='text-lg font-semibold px-6'>Available Timing</p>
         <div className='mt-5 space-y-1'>
           {
-            show.dateTime[date].map((item, index) => (
+            show.dateTime[date].map((item,) => (
               <div key={item.time} onClick={() => setSelectedTime(item)} className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === item.time ? 'bg-primary text-white' : 'hover:bg-primary/20'}`}>
                 <ClockIcon className='w-4 h-4' />
                 {/* alternative way 1 */}
@@ -48,6 +84,42 @@ const SeatLayout = () => {
             ))
           }
         </div>
+      </div>
+
+      {/* seats Layout */}
+
+      <div className='relative flex-1 flex flex-col items-center max-md:mt-16'>
+        <BlurCircle top='-100px' left='-100px' />
+        <BlurCircle bottom='0px' right='0px' />
+        <h1 className='text-2xl font-semibold mb-4'> Select your seat</h1>
+        <img src={assets.screenImage} alt="screen" />
+        <p className='text-gray-400 text-sm mb-6'>SCREEN SIDE</p>
+
+        <div className='flex flex-col items-center mt-10 text-xs text-gray-300'>
+          <div className='grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6'>
+            {
+              groupRows[0].map(row => renderSeats(row))
+            }
+          </div>
+        
+        
+          <div className='grid grid-cols-2 gap-11'>
+          {
+            groupRows.slice(1).map((group, idx) => (
+              <div key={idx}>
+                {group.map(row => renderSeats(row))}
+              </div>
+            ))
+          }
+          </div>
+        </div>
+
+        <button onClick={() => navigate('/my-bookings')} className='flex items-center gap-1 mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer active:scale-95'>
+          Proceed to CheckOut
+          <ArrowRightIcon strokeWidth={3} className='w-4 h-4' />
+        </button>
+
+
       </div>
 
     </div>
